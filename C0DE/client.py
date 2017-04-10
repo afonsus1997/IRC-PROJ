14779cf67c5808b0fd69544bb95ab221f0b66783
Afonso Muralha <afonsomuralha@gmail.com>
2017-04-10 16:57:33 +0100

    did some edits

--- C0DE/client.py
+++ C0DE/client.py
@@ -1,8 +1,12 @@
 import socket
+import sys
+import select
 
+
+SERVER_IP   = '127.0.0.1'
 SERVER_PORT=0
-server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+inputs = [sock, sys.stdin]
 
 def splashscreen():
 	print("\n")
@@ -21,17 +25,37 @@ def splashscreen():
 
 
 def startup():
-	SERVER_PORT = int(input("Input Port > "))
-	try:
-		server.bind(('',SERVER_PORT))
-	except:
-		raise Exception('Error Binding port')
-		exit()
-	print("\nServer Started...\n")
+
+	splashscreen()
+	print("Connected to server...\n")
+print("----------------------------------------------\n\n")
 
 
-splashscreen()
+
+
+
 startup()
+
+print("Input message to server:")
+
+while True:
+  ins, outs, exs = select.select(inputs,[],[])
+  #select devolve para a lista ins quem esta a espera de ler
+  
+  for i in ins:
+    # i == sys.stdin - alguem escreveu na consola, vamos ler e enviar
+    if i == sys.stdin:
+      # sys.stdin.readline() le da consola
+      msg = sys.stdin.readline()
+      # envia mensagem da consola para o servidor
+      sock.sendto(msg.encode(),(SERVER_IP,SERVER_PORT))
+    # i == sock - o servidor enviou uma mensagem para o socket
+    elif i == sock:
+      (msg,addr) = sock.recvfrom(1024)
+      print("\n\nMessage received from server: " + msg.decode() + "\n")
+      print("----------------------------------------------\n\n")
+      print("Input message to server:")
+
+
 server.close()
 print("Server Stopped...")
-
